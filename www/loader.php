b5ce48b8073ac70d67ba4bd52c9ae43fd9b10bf1
Mark Shust <mark@shust.com>
2017-02-07 07:13:13 -0500

    Standardized formatting

--- www/loader.php
+++ www/loader.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+require_once(__DIR__ . '/../vendor/autoload.php');
 
-$dotenv = new Dotenv\Dotenv(dirname('../'));
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
 $dotenv->load();
 
 $db_host = getenv('DB_HOST');
